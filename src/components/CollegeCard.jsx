b6943046516d5eba176a95fcd6ab937fe92884e5
Domatiara <michellnandara2000@gmail.com>
2025-06-18 14:57:57 +0000

    Trending colleges integration

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -1,9 +1,13 @@
-export default function CollegeCard() {
+import { imageBaseURL } from "../api/client";
+
+export default function CollegeCard({ college }) {
     return (
         <div>
-            <img src={'https://images.unsplash.com/photo-1603437119287-4a3732b685f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} alt="College Card" />
-            <h1>Harvard University</h1>
-            <h2>Cambridge, Massachusetts, UK</h2>
+            <img
+                src={`${imageBaseURL}/${college.image}`}
+                alt={college.name} />
+            <h1>{college.name}</h1>
+            <h2>{college.address}</h2>
         </div>
     );
 }
